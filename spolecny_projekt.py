b619de783cfe2c39c1c436b0763385c12dc1c384
Adam Laufer <kvikan.laufer@gmail.com>
2026-03-19 19:06:45 +0100

    final_v

--- spolecny_projekt.py
+++ spolecny_projekt.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 
+
 class Postavicka:
     def __init__(self):
         self.x = 450
@@ -33,7 +34,12 @@ class Lod:
         pass
 
     def pusteni_jidla(self):
-        pass
+        self.pocitadlo_pusteni_jidla += 1
+        if self.pocitadlo_pusteni_jidla == 60:
+            seznam_jidel.append(Jidlo(self.x,self.y))
+            self.pocitadlo_pusteni_jidla = 0
+
+
 
 
 class Jidlo:
@@ -42,16 +48,24 @@ class Jidlo:
         self.y = y
         self.rect = pygame.rect.Rect(self.x, self.y, 50, 50)
         self.pocitadlo = 0
-        self.image = None
+        self.image = pygame.image.load("cervenyhrac.png")
+        self.image = pygame.transform.scale(self.image,(50,50))
 
-    def ukaz():
-        pass
+    def ukaz(self):
+        okno.blit(self.image,self.rect)
 
     def pohyb(self):
-        pass
+        self.pocitadlo += 1
+        if self.pocitadlo == 10:
+            self.y += 10
+            self.pocitadlo = 0
+            self.rect = pygame.rect.Rect(self.x, self.y, 50, 50)
 
     def kolize_s_postavickou(self):
-        pass
+        global skore
+        if self.rect.colliderect(postavicka.rect):
+            skore += 1
+            seznam_jidel.remove(self)
 
 
 
@@ -59,6 +73,7 @@ skore = 0
 seznam_jidel = []
 postavicka = Postavicka()
 lod= Lod()
+
 
 okno = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
@@ -68,6 +83,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
 
     zmacknuti = pygame.key.get_pressed()
 
